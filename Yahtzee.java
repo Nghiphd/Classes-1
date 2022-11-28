9fecc2ba04cd5ec77a0c990f78a20ca9c361dce5
Nghi Phan <nghi.phan@mrjaffesclass.com>
2022-11-28 11:50:46 -0800

    not-done

--- Yahtzee.java
+++ Yahtzee.java
@@ -1,3 +1,4 @@
+import java.util.Map;
 
 /**
  * Write a description of class Yahtzee here.
@@ -30,23 +31,49 @@ public class Yahtzee
         if(dieNumber == 1) {
             die1.roll();
         } else if (dieNumber == 2) {
-            die1.roll();
             die2.roll();
         } else if (dieNumber == 3) {
-            die1.roll();
-            die2.roll();
             die3.roll();
-        } else {
-            die1.roll();
-            die2.roll();
-            die3.roll();
+        } else if (dieNumber == 4) {
             die4.roll();
+        } else {
+            die5.roll();
         }
     }
     public String summerize() {
-        return ("1-" + die1.value + "; 2-" + die2.value + "; 3-" + die3.value + "; 4-" + die4.value + "; 5-" + die5.value + ";");
+        int val1 = 0;
+        int val2 = 0;
+        int val3 = 0;
+        int val4 = 0;
+        int val5 = 0;
+        int val6 = 0;
+        val(1, val1);
+        val(2, val2);
+        val(3, val3);
+        val(4, val4);
+        val(5, val5);
+        String vals = val1+" "+val2+" "+val3+" "+val4+" " +val5+" ";
+        return(vals);        
+        
     }
     public String toString() {
         return("Dice Values: " + die1.value + " " + die2.value + " " + die3.value + " " + die4.value + " " + die5.value); 
+    }
+    public void val(int val, int count) {
+        if(die1.value == val) {
+            count++;
+        } 
+        if(die2.value == val) {
+            count++;
+        } 
+        if(die3.value == val) {
+            count++;
+        } 
+        if(die4.value == val) {
+            count++;
+        } 
+        if (die5.value == val) {
+            count++;
+        }
     }
 }
